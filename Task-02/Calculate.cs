21d64799fdbe951eab4e8e3aac61b97f8df117a1
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-12-02 10:21:28 +0545

    Add Task-02: Custom delegate implementation including Add and Subtract operations

---
+++ Task-02/Calculate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    //internal class Calculate
+         
+        public class Calculate
+        {
+        public static int Add(int x, int y)=> x+y;
+        public static int Sub(int x, int y) => x-y;
+        }
+    }
+
---
+++ Task-02/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_02
+{
+    public delegate int CalculateFunc(int x, int y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculateFunc caldel;
+
+            caldel = Calculate.Add;
+            Console.WriteLine($"Add  of :{caldel(10, 5)}");
+
+            caldel = Calculate.Sub;
+            Console.WriteLine($"Sub of :{caldel(10, 5)}");
+        }
+    }
+}
